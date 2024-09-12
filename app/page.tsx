b8e15e322bdf7f221891198cf1e8fc68ba3ffe9f
gericke98 <sgerickee@gmail.com>
2024-09-12 19:35:06 +0200

    ui minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { InputComponent } from "./components/inputComponent";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-black-pattern">
-      <div className="bg-white flex flex-col lg:w-[30%] w-[85%] rounded-3xl items-center py-5 px-10">
+      <div className="bg-white flex flex-col lg:w-[30%] w-[85%] rounded-3xl items-center py-5 lg:px-6 px-4">
         <InputComponent />
       </div>
     </main>
